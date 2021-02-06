861c942dce6967cef91e4f7f14b44d654f59965e
Josue Banega <77970077+jbanega@users.noreply.github.com>
2021-02-06 17:04:51 -0400

    Adding files from Visual Studio Code

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // typing animation script
+
+    var typed = new Typed(".typing", {
+        strings: ["Mech. Engineer", "Developer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    })
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Mech. Engineer", "Developer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    })
+
+    // Slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+    
+    // Toggle menu/ navbar script
+    $('.menu-bin').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-bin i').toggleClass("active");
+    });
+});
